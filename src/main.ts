1f189281080974f467b2756c63971b77fdad5ef8
Nha <hellonha228@gamil.com>
2026-09-15 22:58:05 +0700

    Added Swagger(OpenAPI) still need more testing on API

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+  .setTitle('Cafe POS API')
+  .setDescription('This Backend API for our POS System')
+  .setVersion('1.0')
+  .build()
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document);
+
   const port = 3000;
   await app.listen(port);
   Logger.log (`Application is running on: http://localhost:${port}`);
+  Logger.log (`API is running on: http://localhost:${port}/api`);
 }
 bootstrap();
